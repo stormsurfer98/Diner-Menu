18a68fb4e9fe71f2cbcdc6004df216e905627ce3
Shrey Gupta <gupta.lego@gmail.com>
2015-10-03 19:38:44 -0400

    Update JavaScript

--- js/main.js
+++ js/main.js
@@ -4,9 +4,19 @@ function updatePage(event) {
 		opacity = element.nextElementSibling.style.opacity;
 		element.nextElementSibling.style.opacity = .1;
 		setTimeout(function() { element.nextElementSibling.style.opacity = opacity; }, 250); 
-	} else {
+	} else if(element.parentElement.nextElementSibling != null) {
 		opacity = element.parentElement.nextElementSibling.style.opacity;
 		element.parentElement.nextElementSibling.style.opacity = .1;
 		setTimeout(function() { element.parentElement.nextElementSibling.style.opacity = opacity; }, 250); 
+	} else if(element.parentElement.parentElement.nextElementSibling != null) {
+		opacity = element.parentElement.parentElement.nextElementSibling.style.opacity;
+		element.parentElement.parentElement.nextElementSibling.style.opacity = .1;
+		setTimeout(function() { element.parentElement.parentElement.nextElementSibling.style.opacity = opacity; }, 250); 
+	} else {
+		element = document.getElementById("start");
+		console.log(element);
+		opacity = element.style.opacity;
+		element.style.opacity = .1;
+		setTimeout(function() { element.style.opacity = opacity; }, 250); 
 	}
 }
