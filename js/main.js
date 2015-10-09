e6d0f79c2f591037890557a7fa218a1859d8a1d3
Shrey Gupta <gupta.lego@gmail.com>
2015-10-09 14:59:07 -0400

    Update JavaScript

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 function updatePage(event) {
 	element = event.target;
-	if(element.nextElementSibling != null) {
+	if(element.firstElementChild !== null) {
+		opacity = element.firstElementChild.style.opacity;
+		element.firstElementChild.style.opacity = 0.1;
+		setTimeout(function() { element.firstElementChild.style.opacity = opacity; }, 250); 
+	} else if(element.nextElementSibling !== null) {
 		opacity = element.nextElementSibling.style.opacity;
-		element.nextElementSibling.style.opacity = .1;
+		element.nextElementSibling.style.opacity = 0.1;
 		setTimeout(function() { element.nextElementSibling.style.opacity = opacity; }, 250); 
-	} else if(element.parentElement.nextElementSibling != null) {
+	} else if(element.parentElement.nextElementSibling !== null) {
 		opacity = element.parentElement.nextElementSibling.style.opacity;
-		element.parentElement.nextElementSibling.style.opacity = .1;
+		element.parentElement.nextElementSibling.style.opacity = 0.1;
 		setTimeout(function() { element.parentElement.nextElementSibling.style.opacity = opacity; }, 250); 
-	} else if(element.parentElement.parentElement.nextElementSibling != null) {
+	} else if(element.parentElement.parentElement.nextElementSibling !== null) {
 		opacity = element.parentElement.parentElement.nextElementSibling.style.opacity;
-		element.parentElement.parentElement.nextElementSibling.style.opacity = .1;
+		element.parentElement.parentElement.nextElementSibling.style.opacity = 0.1;
 		setTimeout(function() { element.parentElement.parentElement.nextElementSibling.style.opacity = opacity; }, 250); 
 	} else {
 		element = document.getElementById("start");
 		console.log(element);
 		opacity = element.style.opacity;
-		element.style.opacity = .1;
+		element.style.opacity = 0.1;
 		setTimeout(function() { element.style.opacity = opacity; }, 250); 
 	}
 }
